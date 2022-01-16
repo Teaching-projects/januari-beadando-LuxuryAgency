c71f426df8a0635b8902cf9e15ca4bbaee15eb05
LuxuryAgency <84640982+LuxuryAgency@users.noreply.github.com>
2022-01-16 17:29:42 +0100

    Add files via upload

--- beadando_ vegleges.py
+++ beadando_ vegleges.py
@@ -1,6 +1,13 @@
 
 """A program kiszámítja egy ház szobáinak kifestéséhez szükséges festék mennyiségét és annak árát"""
 
+def muvelet1():
+     szobak.append(round(float(szobakmeretenyers[0])*float(szobakmeretenyers[1]))+float(szobakmeretenyers[0])**2),2
+    
+def muvelet2():
+    szobak.append(round(float(szobakmeretenyers[0])*float(szobakmeretenyers[1])+(2*(float(szobakmeretenyers[0])*float(szobakmeretenyers[2])))+(2*(float(szobakmeretenyers[1])*float(szobakmeretenyers[2]))),1))
+     
+     
 szobakmerete=input("Add meg a szobák adatait(6x3 ,6x3x6):").split(",")
 szobak=[]  
 
@@ -8,9 +15,9 @@ for i in szobakmerete:
     szobakmeretenyers = i.split("x")
     
     if len(szobakmeretenyers) == 2:
-        szobak.append(round(float(szobakmeretenyers[0])*float(szobakmeretenyers[1]))+float(szobakmeretenyers[0])**2),2
+        muvelet1()
     else:
-        szobak.append(round(float(szobakmeretenyers[0])*float(szobakmeretenyers[1])+(2*(float(szobakmeretenyers[0])*float(szobakmeretenyers[2])))+(2*(float(szobakmeretenyers[1])*float(szobakmeretenyers[2]))),1))
+        muvelet2()
 
 liter = float(sum(szobak)) /8.33
 litervegleges = round(liter,2)
@@ -19,7 +26,7 @@ litervegleges = round(liter,2)
 színárak = [2000,2200,2400,2600,2800]
 Árak= []
 
-for szamlalas in range(len(szobakmeretenyers)):
+for szamlalas in range(len(szobakmeretenyers)-1):
     
     print("Milyen színűre szeretnéd a(z)",(szamlalas+1),".  szobát? Fekete:2000ft/l (0) ,Barna:2200ft/l (1),Kék:2400ft/l (2),Zöld:2600ft/l (3),Fehér:2800ft/l (4). A színek sorszámával válaszolj!")
     s3 = int(input())
